318b71f2de376a95e08c9f15fef9f5f66ba00848
A. Lloyd Flanagan <aflanagan@bhmginc.com>
2015-02-13 21:45:03 +0000

    minor cleanup

--- count_uniq.py
+++ count_uniq.py
@@ -6,24 +6,21 @@ from collections import defaultdict
 def count_uniq_lines(input_stream):
     d = defaultdict(int)  # default 0
 
-    for line in sys.stdin:
+    for line in input_stream:
         line = line[:-1]
-        d[line] = d[line] + 1
+        d[line] += 1
 
-    sum = 0
-    for l in d.keys():
-        sum = sum + d[l]
-        print("Total # of lines is {}.".format(sum([v for k, v in d])))
+    sys.stderr.write("Lines in file: {:,}.\n".format(sum([d[k] for k in d])))
 
-    k = d.keys()
-    k.sort()
+    k = [k for k in d]
+    k.sort(key=str.lower)
     for b in k:
         print("{}:  {}".format(b, d[b]))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         with open(sys.argv[1], "r") as txt_in:
-            print("Reading from {}.".format(sys.argv[1]))
+            sys.stderr.write("Reading from {}.\n".format(sys.argv[1]))
             count_uniq_lines(txt_in)
     else:
         count_uniq_lines(sys.stdin)
