767ba20b62be995fb275ac90886f85cb86086ff3
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-02-14 17:39:26 -0500

    Cleanups for shellcheck; add ssh_init

--- functions.bash
+++ functions.bash
@@ -10,6 +10,8 @@ TERM_BOLD_OFF=$(tput -T xterm rmso)
 TERM_UL_ON=$(tput -T xterm smul)
 TERM_UL_OFF=$(tput -T xterm rmul)
 
+export TERM_BOLD_ON TERM_BOLD_OFF TERM_UL_OFF TERM_UL_ON
+
 #a lot of useful bash functions
 bldpath() {
 	# bldpath VARNAME DIR [before|after]
@@ -51,14 +53,14 @@ bldpath() {
 
 pathmunge() {
 	[[ $# -gt 0 ]] || { cat <<-EOF
-	Usage: ${FUNCNAME} dir_path [before|after]
+	Usage: ${FUNCNAME[0]} dir_path [before|after]
 		   adds dir_path to PATH variable
 		   adds to end unless 'before' specified.
 	EOF
 						return 1
 						}
 	[[ -d "$1" ]] || {
-		echo "${FUNCNAME} ERROR: path not found [$1]" >&2
+		echo "${FUNCNAME[0]} ERROR: path not found [$1]" >&2
 		return 2
 	}
 	bldpath PATH "$1" "$2"
@@ -99,7 +101,7 @@ pathrm() {
             fi
         fi
     done
-    
+
     export PATH="${newpath}"
     hash -r
 }
@@ -107,7 +109,7 @@ pathrm() {
 manmunge() {
 	if [[ $# -lt 1 ]]; then
 		cat <<-EOF
-	Usage: ${FUNCNAME} ${TERM_UL_ON}directory${TERM_UL_OFF} [before|after]
+	Usage: ${FUNCNAME[0]} ${TERM_UL_ON}directory${TERM_UL_OFF} [before|after]
 			Adds ${TERM_UL_ON}directory${TERM_UL_OFF} to MANPATH environment variable.
 			${TERM_UL_ON}directory${TERM_UL_OFF} is added to end unless 'before' is specified.
 	EOF
@@ -118,29 +120,30 @@ manmunge() {
 
 ldlibmunge() {
 	[[ $# -gt 0 ]] || { cat <<-EOF
-		Usage: ${FUNCNAME} ${TERM_UL_ON}directory${TERM_UL_OFF} [before|after]
+		Usage: ${FUNCNAME[0]} ${TERM_UL_ON}directory${TERM_UL_OFF} [before|after]
 			   Adds ${TERM_UL_ON}directory${TERM_UL_OFF} to LD_LIBRARY_PATH environment variable."
 			   ${TERM_UL_ON}directory${TERM_UL_OFF} is added to end unless 'before' is specified."
 		EOF
-		return -1
+		return 1
 	}
 	bldpath LD_LIBRARY_PATH "$1" "$2"
 }
 
 myps() {
-#list processes running under current user's name, except for some system stuff
-#cat causes whole line to print, wrapped
-    ps -fu $(whoami) | grep -v -e '/usr/libexec/' -e 'dbus' -e 'gnome-pty-helper' -e 'ibus-daemon' -e 'keyring-daemon' | cat
+	#list processes running under current user's name, except for some system stuff
+	#cat causes whole line to print, wrapped
+	# shellcheck disable=SC2009
+    ps -fu "$(whoami)" | grep -v -e '/usr/libexec/' -e 'dbus' -e 'gnome-pty-helper' -e 'ibus-daemon' -e 'keyring-daemon' | cat
 }
 
 fixldpath() {
 #eliminates duplicates from LD_LIBRARY_PATH variable
     local newldpath
-    for DIR in $(echo ${LD_LIBRARY_PATH} | tr ':' '\n')
+    for DIR in $(echo "${LD_LIBRARY_PATH}" | tr ':' '\n')
     do
-        bldpath newldpath ${DIR}
+        bldpath newldpath "${DIR}"
     done
-    export LD_LIBRARY_PATH=${newldpath}
+    export LD_LIBRARY_PATH="${newldpath}"
 }
 
 #TODO: modify this so that functions can have special doc comment as first line
@@ -155,7 +158,7 @@ functions() {
 
 findtextin() {
 	[[ $# -lt 3 ]] && { cat <<-EOF
-		Usage: ${FUNCNAME} <start-dir> '<file-pattern>' <search-text>
+		Usage: ${FUNCNAME[0]} <start-dir> '<file-pattern>' <search-text>
 			   <start-dir>: top-level directory, it & all subs will be searched.
 			   <search-text>: text string to search for in each file that matches
 			                  <file-pattern>
@@ -173,7 +176,7 @@ find_no_svn() {
 
 find_no_svn_grep() {
     if  [[ $# -eq 0 ]]; then
-        echo "${FUNCNAME} ${TERM_UL_ON}[path-spec]${TERM_UL_OFF} ${TERM_UL_ON}search_term${TERM_UL_OFF} ${TERM_UL_ON}additional_find_options${TERM_UL_OFF}"
+        echo "${FUNCNAME[0]} ${TERM_UL_ON}[path-spec]${TERM_UL_OFF} ${TERM_UL_ON}search_term${TERM_UL_OFF} ${TERM_UL_ON}additional_find_options${TERM_UL_OFF}"
         echo "    grep selected files for lines containing ${TERM_UL_ON}search_term${TERM_UL_OFF}."
         echo "    ${TERM_UL_ON}additional_find_options${TERM_UL_OFF} are any valid options for find (1)."
         echo "    ${TERM_UL_ON}path_spec${TERM_UL_OFF} defaults to current directory."
@@ -186,7 +189,7 @@ find_no_svn_grep() {
 		local options="-type f"
 		# how to do this space-safe? Build an array?
 		[[ -z "$*" ]] || options="${options} $*"
-		echo find "${DIR}" -name \'.svn\' -prune -o -name \'.git\' -prune -o \( "${options}" \) -exec grep -IH \"${search_term}\" \'{}\' +
+		echo find "${DIR}" -name \'.svn\' -prune -o -name \'.git\' -prune -o \( "${options}" \) -exec grep -IH \""${search_term}"\" \'{}\' +
 		find "${DIR}" -name '.svn' -prune -name '.git' -prune -o \( "${options}" \) -exec grep -IH "${search_term}" '{}' +
 
     fi
@@ -194,7 +197,7 @@ find_no_svn_grep() {
 
 find_no_svn_igrep() {
     if  [[ $# -eq 0 ]]; then
-        echo "${FUNCNAME} ${TERM_UL_ON}[path-spec]${TERM_UL_OFF} ${TERM_UL_ON}search_term${TERM_UL_OFF} ${TERM_UL_ON}additional_find_options${TERM_UL_OFF}"
+        echo "${FUNCNAME[0]} ${TERM_UL_ON}[path-spec]${TERM_UL_OFF} ${TERM_UL_ON}search_term${TERM_UL_OFF} ${TERM_UL_ON}additional_find_options${TERM_UL_OFF}"
         echo "    case-insensitive grep selected files for lines containing ${TERM_UL_ON}search_term${TERM_UL_OFF}."
         echo "    ${TERM_UL_ON}additional_find_options${TERM_UL_OFF} are any valid options for find (1)."
     else
@@ -206,7 +209,7 @@ find_no_svn_igrep() {
 
         local search_term="$1"
         shift
-        echo find "${DIR}" -name '.svn' -prune -o -name '.git' -prune -o \( "$@" \) -exec grep -iIH \"${search_term}\" \'{}\' \\\;
+        echo find "${DIR}" -name '.svn' -prune -o -name '.git' -prune -o \( "$@" \) -exec grep -iIH \""${search_term}"\" \'{}\' \\\;
         find "${DIR}" -name '.svn' -prune -o -name '.git' -prune -o \( "$@" \) -exec grep -iIH "${search_term}" '{}' \;
     fi
 }
@@ -214,20 +217,24 @@ find_no_svn_igrep() {
 
 # -----------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------
-
 #appears to be no way to turn off unalias error if alias doesn't exist
 unalias ll 2>/dev/null
+
+# shellcheck disable=SC2012
 ll() {
     ls -Alh "$@"
 }
 
 #one-line functions must have ';' before final }
 lrt() { ls -lrt "$@"; }
+# shellcheck disable=SC2012
 lrtail() { ls -lrt "$@" | tail; }
+# shellcheck disable=SC2012
 lrtc() { ls -lrt "$@" | commall.py; }
 
 largest() {
     #or use ls -S
+		# shellcheck disable=SC2012
     ls -l "$@" | sort -r -g -k5
 }
 
@@ -250,28 +257,9 @@ chkpartio() {
     vmstat -p sda3
 }
 
-lnall() {
-    if [[ $# -ne 3 ]]; then
-        echo "Usage: ${FUNCNAME} path filename linkname"
-        echo '       finds all files named {filename} in {path} or subdirectories of {path}'
-        echo '       creates a symbolic link with name {linkname} that points to {filename}'
-        echo '       link is created in same directory as the file to which it points'
-        return 1
-    fi
-    echo 'finding...'
-    for FILE in $(find "$1" -name "$2" -a ! -type d)
-    do
-    #this creates extraneous links if FILE is a directory, not sure why
-        cd "${FILE%/*}"  # dirname
-        echo "$3 --> ${FILE}"
-        ln -s "${FILE##*/}" "$3"  # basename
-        cd -
-    done
-}
-
 execsql() {
   if  [[ $# -eq 0 ]]; then
-    echo "${FUNCNAME} ${TERM_UL_ON}sql_file${TERM_UL_OFF} ${TERM_UL_ON}additional_psql_options${TERM_UL_OFF}"
+    echo "${FUNCNAME[0]} ${TERM_UL_ON}sql_file${TERM_UL_OFF} ${TERM_UL_ON}additional_psql_options${TERM_UL_OFF}"
     echo "    Call psql to execute ${TERM_UL_ON}sql_file${TERM_UL_OFF} in database netinformer as user apache."
   else
     local SQL_FILE="$1"
@@ -282,7 +270,7 @@ execsql() {
 
 domysql() {
     if  [[ $# -eq 0 ]]; then
-        echo "${FUNCNAME} ${TERM_UL_ON}db_name${TERM_UL_OFF} ${TERM_UL_ON}sql_statement${TERM_UL_OFF}"
+        echo "${FUNCNAME[0]} ${TERM_UL_ON}db_name${TERM_UL_OFF} ${TERM_UL_ON}sql_statement${TERM_UL_OFF}"
         echo "    Execute ${TERM_UL_ON}sql_statement${TERM_UL_OFF} with mysql using database ${TERM_UL_ON}db_name${TERM_UL_OFF}."
     else
         mysql -u root -p <<HERE
@@ -294,28 +282,16 @@ HERE
 
 execmysql() {
   if  [[ $# -eq 0 ]]; then
-    echo "${FUNCNAME} ${TERM_UL_ON}sql_file${TERM_UL_OFF}"
+    echo "${FUNCNAME[0]} ${TERM_UL_ON}sql_file${TERM_UL_OFF}"
     echo "    Execute SQL statements in ${TERM_UL_ON}sql_file${TERM_UL_OFF} with mysql."
   else
     mysql -u root -p < "${1}"
   fi
 }
 
-lgv() {
-    if [[ $# -lt 1 ]]; then
-        echo "${FUNCNAME} some-pattern [ls-options] -- list all files that don't match some-pattern"
-    elif [[ $# -eq 1 ]]; then
-        ls | grep -v "$*"
-    else
-        PATTERN=$1
-        shift
-        ls "$@" | grep -v "${PATTERN}"
-    fi
-}
-
 llgv() {
     if [[ $# -lt 1 ]]; then
-        echo "${FUNCNAME} some-pattern [ls-options] -- list (long form) all files that don't match some-pattern"
+        echo "${FUNCNAME[0]} some-pattern [ls-options] -- list (long form) all files that don't match some-pattern"
     else
         lgv "$@" -l
     fi
@@ -324,23 +300,23 @@ llgv() {
 every() {
 #TODO: detect, handle bad input
     if [[ $# -lt 3 ]]; then
-        echo "Usage: ${FUNCNAME} _seconds_ do _command_"
+        echo "Usage: ${FUNCNAME[0]} _seconds_ do _command_"
         echo "       where _seconds_ is time, and _command_ is a shell command"
         echo "       note: if _command_ contains multiple commands or redirection, quote it"
         return 1
     fi
     local secs=$1
     shift
-    if [[ $1 == do ]]; then
+    if [[ $1 == 'do' ]]; then
         shift
     fi
-    local cmd="$@"
+    local cmd="$*"
     #echo cmd is ${cmd}
     while true
     do
         #echo evaluating "${cmd}"
         eval "${cmd}"
-        sleep ${secs}
+        sleep "${secs}"
     done
 }
 
@@ -348,17 +324,17 @@ with() {
     local ON=${TERM_UL_ON}
     local OFF=${TERM_UL_OFF}
     if [[ $# -lt 3 ]]; then
-        echo "Usage: ${FUNCNAME} ${ON}dir_name${OFF} do ${ON}commands...${OFF}"
+        echo "Usage: ${FUNCNAME[0]} ${ON}dir_name${OFF} do ${ON}commands...${OFF}"
         echo "       executes ${ON}commands${OFF}... with current directory set to ${ON}dir_name${OFF}"
         echo "       Does not change user's current directory."
         return 1
     fi
     local DIR="$1"
     shift
-    if [[ "$1" == do ]]; then
+    if [[ "$1" == 'do' ]]; then
         shift
     fi
-    local CMDS="$@"
+    local CMDS="$*"
     pushd "${DIR}" > /dev/null
     eval "${CMDS}"
     popd > /dev/null
@@ -369,17 +345,18 @@ sum_size() {
     #i.e. "sum_size -h" should generate usage message
     if [[ $# -lt 1 ]]; then
         #because "du -c | tail -n 1" will sum all files in directory tree, take arbitrarily long time :-(
-        echo "Usage: ${FUNCNAME} [args] files.."
+        echo "Usage: ${FUNCNAME[0]} [args] files.."
         echo "       Prints total space taken by files in file list. Accepts arguments as defined in du(1)."
         return 1
     fi
-    local TOTAL=$(du -c "$@" | tail -n 1)
-    echo ${TOTAL} | cut -f1 -d" "
+    local TOTAL
+    TOTAL=$(du -c "$@" | tail -n 1)
+    echo "${TOTAL}" | cut -f1 -d" "
 }
 
 h2d() {
     if [[ $# -lt 1 ]]; then
-        echo "Usage: ${FUNCNAME} hex_number"
+        echo "Usage: ${FUNCNAME[0]} hex_number"
         echo "       converts hex_number from hexadecimal to integer, prints result."
         return 1
     fi
@@ -391,70 +368,69 @@ h2d() {
 }
 
 # tempting to create h2o(), but would it ever get used?
+# warning shouldn't apply to global functions to be used outside of script.
+# but how to tell shellcheck?
+# shellcheck disable=SC2120,SC2119
+{
+	o2d() {
+	  [[ $# -ne 1 ]] && {
+	      echo "Usage: ${FUNCNAME[0]} octal_number"
+	      echo "       converts octal_number from octal to integer, prints result."
+	      return 1
+	  }
+	  local valid_number
+	  valid_number='^[0-7]+$'
 
-o2d() {
-  if [[ $# -lt 1 ]]; then
-      echo "Usage: ${FUNCNAME} octal_number"
-      echo "       converts octal_number from octal to integer, prints result."
-      return 1
-  elif [[ $1 == 0 ]]; then
-      # some invalid input sets octnum to 0, so we have to special case actual 0
-      # if we want to catch invalid input
-      echo 0
-      return
-  fi
-  
-  declare -i octnum=$1
-  # TODO: check for digit > 7
-  if (( ${octnum} == 0 )); then
-    echo "ERROR: input not a number" >&2
-    o2d
-  else
-    dc -e "8i${octnum}p"
-  fi
+	  [[ "$1" =~ ${valid_number} ]] || {
+	      echo "ERROR: input not an octal number" >&2
+	      o2d  # show usage
+	      return 1
+	  }
+
+	  dc -e "8i${1}p"
+	}
+
+	d2h() {
+	  if [[ $# -lt 1 ]]; then
+	      echo "Usage: ${FUNCNAME[0]} number"
+	      echo "       converts decimal number to hexadecimal, prints result."
+	      return 1
+	  elif [[ $1 == 0 ]]; then
+	      # see comment in o2d()
+	      echo "0"
+	      return
+	  fi
+	  declare -i INPUT="$1"
+
+	  if [[ ${INPUT} -eq 0 ]]; then
+	    # bad conversion, show help
+	    echo "ERROR: input is not a number" >&2
+	    d2h
+	  else
+	    dc -e "16o${INPUT}p"
+	  fi
+	}
+
+	d2o() {
+	  if [[ $# -lt 1 ]]; then
+	      echo "Usage: ${FUNCNAME[0]} number"
+	      echo "       converts decimal number to octal, prints result."
+	      return 1
+	  elif [[ $1 == 0 ]]; then
+	      # see comment in o2d()
+	      echo "0"
+	      return
+	  fi
+
+	  local -i INPUT="${1}"
+	  if [[ ${INPUT} -eq 0 ]]; then
+	    echo "ERROR: input is not a number" >&2
+	    d2o
+	  else
+	    dc -e "8o${INPUT}p"
+	  fi
+	}
 }
-
-d2h() {
-  if [[ $# -lt 1 ]]; then
-      echo "Usage: ${FUNCNAME} number"
-      echo "       converts decimal number to hexadecimal, prints result."
-      return 1
-  elif [[ $1 == 0 ]]; then
-      # see comment in o2d()
-      echo "0"
-      return
-  fi
-  declare -i INPUT="$1"
-  
-  if [[ ${INPUT} -eq 0 ]]; then
-    # bad conversion, show help
-    echo "ERROR: input is not a number" >&2
-    d2h
-  else
-    dc -e "16o${INPUT}p"
-  fi
-}
-
-d2o() {
-  if [[ $# -lt 1 ]]; then
-      echo "Usage: ${FUNCNAME} number"
-      echo "       converts decimal number to octal, prints result."
-      return 1
-  elif [[ $1 == 0 ]]; then
-      # see comment in o2d()
-      echo "0"
-      return
-  fi
-
-  local -i INPUT="${1}"
-  if [[ ${INPUT} -eq 0 ]]; then
-    echo "ERROR: input is not a number" >&2
-    d2o
-  else
-    dc -e "8o${INPUT}p"
-  fi
-}
-
 x2d () {
     #turns out when I try to remember "h2d" my brain comes up with
     #"x2d" instead. Go figure.
@@ -463,7 +439,7 @@ x2d () {
 
 curl_get() {
     if [[ $# -lt 1 ]]; then
-        echo "Usage: ${FUNCNAME} URL [destination_file_name]"
+        echo "Usage: ${FUNCNAME[0]} URL [destination_file_name]"
         echo "       Retrieves a URL using curl and saves it to a file. Handles errors intelligently"
         return 1
     fi
@@ -483,7 +459,7 @@ curl_get() {
 
 curl_get_missing() {
     if [[ $# -lt 1 ]]; then
-        echo "Usage: ${FUNCNAME} URL [destination_file_name]"
+        echo "Usage: ${FUNCNAME[0]} URL [destination_file_name]"
         echo "       Retrieves a URL using curl and saves it to a file. Handles errors intelligently."
         echo "       Does not retrieve the file if the destination file already exists."
         return 1
@@ -491,7 +467,7 @@ curl_get_missing() {
     if [[ $# -eq 2 ]]; then
         local DEST_FILE="$2"
     else
-        local DEST_FILE= "${THE_URL##*/}"
+        local DEST_FILE="${THE_URL##*/}"
     fi
     if [[ ! -f "${DEST_FILE}" ]]; then
         curl_get "$1" "$2"
@@ -505,7 +481,7 @@ xwhich() {
 
 wing() {
     #verbose causes errors to log, etc.
-    /usr/bin/wing6.0 --verbose "$@" > ${HOME}/log/wing6.log 2>&1 &
+    /usr/bin/wing6.0 --verbose "$@" > "${HOME}/log/wing6.log" 2>&1 &
 }
 
 #get list of files in a zip, dropping all info except file names
@@ -524,18 +500,18 @@ dtree() {
 extra() {
     #switch from home directory to parallel directory on /mnt/extra
     #for when I don't want to work through soft links
-    cd ${PWD/\/home\/aflanagan/\/mnt\/extra}
+    cd $"{PWD/\/home\/aflanagan/\/mnt\/extra}" || exit 1
 }
 
 view_html() {
     case "$@" in
         /*)
-            echo firefox --no-remote --new-window file://localhost"$@" "&"
-            firefox --no-remote --new-window file://localhost"$@" &
+            echo firefox --no-remote --new-window file://localhost"$*" "&"
+            firefox --no-remote --new-window file://localhost"$*" &
             ;;
         *)
-            echo firefox --no-remote --new-window file://localhost${PWD}/"$@" "&"
-            firefox --no-remote --new-window file://localhost${PWD}/"$@" &
+            echo firefox --no-remote --new-window "file://localhost${PWD}/$*" "&"
+            firefox --no-remote --new-window "file://localhost${PWD}/$*" &
             ;;
     esac
 }
@@ -595,7 +571,7 @@ ecompile() {
 
 columns() {
   if [[ $# -gt 0 ]]; then
-    pr -t -T -$1
+    pr -t -T -"$1"
   else
     pr -t -T
   fi
@@ -619,15 +595,21 @@ comp() {
     local COMP_TYPE=9
     local COMP_KEY=9
     _command_offset 0
-    echo ${COMPREPLY[@]}
+    echo "${COMPREPLY[@]}"
 }
 
 npm_packages() {
   # list npm packages without semver
   # should be a better way to do this
-  npm $* ls --depth=0 | grep -v npm | cut -d' ' -f2 | grep -v /usr | grep -v '^$' | cut -d'@' -f1
+  npm "$@" ls --depth=0 | grep -v npm | cut -d' ' -f2 | grep -v /usr | grep -v '^$' | cut -d'@' -f1
 }
 
+ssh-init() {
+	eval "$(ssh-agent)"
+	ssh-add
+	ssh-add ~/.ssh/mgvwdm003_id_rsa
+	ssh-add ~/.ssh/id_rsa.personal
+}
 # Local Variables:
 # indent-tabs-mode: t
 # tab-width: 4
